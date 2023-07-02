6b73ce7655091542ad3907bf1b22f55c050cc4fb
ravel01106 <ravello2002@gmail.com>
2023-07-02 14:26:38 +0100

    feat: the second case tests have passed successfully

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,59 @@
-export function fizzbuzz(array:number[]):string[]{
-  let arrayResult:string[] = [];
-  array.forEach(number => {
+function fizzbuzz(param:number[]):string[];
+function fizzbuzz(param:number):string;
+function fizzbuzz(param:any):any{
+  let isNumber:boolean = typeof param == 'number';
+  let isArray:boolean = Array.isArray(param);
+  let result:string = "";
+  let numStr:string = "";
 
-    let result:string = "";
-    let numStr:string = number.toString();
+  if(isNumber){
+    let num: number = param;
+    numStr = num.toString();
+    if(num % 3 == 0){
+      result += "Fizz";
+    }
+    if(numStr.includes("3")){
+      result += "Fizz";
+    }
+    if(num % 5 == 0){
+      result += "Buzz";
+    }
+    if(numStr.includes("5")){
+      result += "Buzz";
+    }
+    if(result == ""){
+      result = numStr;
+    }
+    return result;
 
-    let isDivisibleByThree:boolean = number % 3 == 0;
-    let isDivisibleByFive:boolean = number % 5 == 0;
-    let containsThree:boolean = numStr.includes("3");
-    let containsFive:boolean = numStr.includes("5");
-    
-    result += isDivisibleByThree ? "Fizz" : "";
-    result += containsThree ? "Fizz" : "";
-    result += isDivisibleByFive ? "Buzz" : "";
-    result += containsFive ? "Buzz" : "";
+  }else if(isArray){
+    let array:number[] = param;
+    let arrayResult:string[] = [];
+    array.forEach(number => {
 
-    let isResultEmpty:boolean = result == "";
-
-    result = isResultEmpty ? numStr : result;
-
-    console.log(result)
-    arrayResult.push(result);
-  });
-
-  return arrayResult;
+      result = "";
+      numStr = number.toString();
+  
+      let isDivisibleByThree:boolean = number % 3 == 0;
+      let isDivisibleByFive:boolean = number % 5 == 0;
+      let containsThree:boolean = numStr.includes("3");
+      let containsFive:boolean = numStr.includes("5");
+      
+      result += isDivisibleByThree ? "Fizz" : "";
+      result += containsThree ? "Fizz" : "";
+      result += isDivisibleByFive ? "Buzz" : "";
+      result += containsFive ? "Buzz" : "";
+  
+      let isResultEmpty:boolean = result == "";
+  
+      result = isResultEmpty ? numStr : result;
+  
+      console.log(result)
+      arrayResult.push(result);
+    });
+  
+    return arrayResult;
+  }
+    return 'Sorry but you must enter a number or array of numbers.';
 }
+export default fizzbuzz;
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,5 @@
 import {describe, test, expect} from 'vitest';
-import {fizzbuzz} from '../index';
+import fizzbuzz from '../index';
 
 
 describe('FizzBuzz test', () => {
@@ -13,5 +13,29 @@ describe('FizzBuzz test', () => {
         expect(fizzbuzz([1 , 9, 10, 3, 5, 30, 35, 53, 135])).toStrictEqual(
             ['1', 'Fizz', 'Buzz', 'FizzFizz', 'BuzzBuzz', 'FizzFizzBuzz', 'FizzBuzzBuzz', 'FizzBuzz', 'FizzFizzBuzzBuzz']
             );
+    });
+    test('if we pass 1 it should return us "1"', () =>{
+        expect(fizzbuzz(1)).toStrictEqual('1');
+    });
+    test('if we pass 3 it should return us "FizzFizz"', () =>{
+        expect(fizzbuzz(3)).toStrictEqual('FizzFizz');
+    });
+    test('if we pass 5 it should return us "BuzzBuzz"', () =>{
+        expect(fizzbuzz(5)).toStrictEqual('BuzzBuzz');
+    });
+    test('if we pass 10 it should return us "Buzz"', () =>{
+        expect(fizzbuzz(10)).toStrictEqual('Buzz');
+    });
+    test('if we pass 6 it should return us "Fizz"', () =>{
+        expect(fizzbuzz(6)).toStrictEqual('Fizz');
+    });
+    test('if we pass 35 it should return us "FizzBuzzBuzz"', () =>{
+        expect(fizzbuzz(35)).toStrictEqual('FizzBuzzBuzz');
+    });
+    test('if we pass 53 it should return us "FizzBuzz"', () =>{
+        expect(fizzbuzz(53)).toStrictEqual('FizzBuzz');
+    });
+    test('if we pass 53 it should return us "FizzBuzz"', () =>{
+        expect(fizzbuzz("Hola")).toStrictEqual('Sorry but you must enter a number or array of numbers.');
     });
 });
