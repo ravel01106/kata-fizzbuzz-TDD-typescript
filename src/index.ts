1b4090c09bf1aba77b4915ef489c481cf6b837fe
ravel01106 <ravello2002@gmail.com>
2023-07-02 15:26:27 +0100

    refactor(index): Change from conditional to ternary and the tests still pass correctly

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,65 @@ function fizzbuzz(param:any):any{
   let isArray:boolean = Array.isArray(param);
   let result:string = "";
   let numStr:string = "";
+  let isDivisibleByThree:boolean = false;
+  let isDivisibleByFive:boolean = false;
+  let containsThree:boolean = false;
+  let containsFive:boolean = false;
+  let isResultEmpty:boolean = false;
+
 
   if(isNumber){
+
     let num: number = param;
     numStr = num.toString();
-    if(num % 3 == 0){
-      result += "Fizz";
-    }
-    if(numStr.includes("3")){
-      result += "Fizz";
-    }
-    if(num % 5 == 0){
-      result += "Buzz";
-    }
-    if(numStr.includes("5")){
-      result += "Buzz";
-    }
-    if(result == ""){
-      result = numStr;
-    }
+
+    isDivisibleByThree = num % 3 == 0;
+    isDivisibleByFive = num % 5 == 0;
+    containsThree = numStr.includes("3");
+    containsFive = numStr.includes("5");
+    
+    result += isDivisibleByThree ? "Fizz" : "";
+    result += containsThree ? "Fizz" : "";
+    result += isDivisibleByFive ? "Buzz" : "";
+    result += containsFive ? "Buzz" : "";
+
+    isResultEmpty = result == "";
+
+    result = isResultEmpty ? numStr : result;
+
     return result;
 
   }else if(isArray){
+
     let array:number[] = param;
     let arrayResult:string[] = [];
+
     array.forEach(number => {
 
       result = "";
       numStr = number.toString();
   
-      let isDivisibleByThree:boolean = number % 3 == 0;
-      let isDivisibleByFive:boolean = number % 5 == 0;
-      let containsThree:boolean = numStr.includes("3");
-      let containsFive:boolean = numStr.includes("5");
+      isDivisibleByThree = number % 3 == 0;
+      isDivisibleByFive = number % 5 == 0;
+      containsThree = numStr.includes("3");
+      containsFive = numStr.includes("5");
       
       result += isDivisibleByThree ? "Fizz" : "";
       result += containsThree ? "Fizz" : "";
       result += isDivisibleByFive ? "Buzz" : "";
       result += containsFive ? "Buzz" : "";
   
-      let isResultEmpty:boolean = result == "";
+      isResultEmpty = result == "";
   
       result = isResultEmpty ? numStr : result;
-  
-      console.log(result)
+
       arrayResult.push(result);
     });
   
     return arrayResult;
+
   }
     return 'Sorry but you must enter a number or array of numbers.';
+    
 }
 export default fizzbuzz;
